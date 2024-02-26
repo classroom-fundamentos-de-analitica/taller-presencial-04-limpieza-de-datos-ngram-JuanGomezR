a24738724b0ef6b2ee8113fe8ca06cbdc18d7cff
Juan Pablo Gómez Reyes <jugomezre@unal.edu.co>
2024-02-26 11:19:45 -0500

    Commit

--- clean_data.py
+++ clean_data.py
@@ -1,41 +1,41 @@
 """Taller evaluable presencial"""
 
+import nltk
 import pandas as pd
 
 
 def load_data(input_file):
-    """Lea el archivo usando pandas y devuelva un DataFrame"""
 
+    data = pd.read_csv(input_file, sep="\t")
+    return data
 
 def create_key(df, n):
-    """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
-
     df = df.copy()
-
-    # Copie la columna 'text' a la columna 'key'
-    # Remueva los espacios en blanco al principio y al final de la cadena
-    # Convierta el texto a minúsculas
-    # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
-    # Remueva puntuación y caracteres de control
-    # Convierta el texto a una lista de tokens
-    # Una el texto sin espacios en blanco
-    # Convierta el texto a una lista de n-gramas
-    # Ordene la lista de n-gramas y remueve duplicados
-    # Convierta la lista de ngramas a una cadena
-    
+    df["key"] = df["text"]
+    df["key"] = (
+        df["key"]
+        .str.strip()
+        .str.lower()
+        .str.replace("-","")
+        .str.translate(
+            str.maketrans("", "", "!\"#$%&'()++,-./:;<=<?@[\\]^_")
+        )
+        .str.replace(".","")
+        .str.split()
+        .str.join("")
+        .apply(lambda x: [x[i:i + n] for i in range(len(x)- n + 1)])
+        .apply(lambda x: sorted(set(x)))
+        .str.join(" ")
+    )
     return df
 
 
 def generate_cleaned_column(df):
-    """Crea la columna 'cleaned' en el DataFrame"""
-
     df = df.copy()
-
-    # Ordene el dataframe por 'key' y 'text'
-    # Seleccione la primera fila de cada grupo de 'key'
-    # Cree un diccionario con 'key' como clave y 'text' como valor
-    # Cree la columna 'cleaned' usando el diccionario
-
+    keys = df.sort_values(by = ["key", "text"]).copy()
+    keys = df.groupby("key").first().reset_index()
+    keys = keys.set_index("key")["text"].to_dict()
+    df["cleaned"] = df["key"].map(keys)
     return df
 
 
